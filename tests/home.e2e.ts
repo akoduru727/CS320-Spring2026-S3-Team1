84652cf8f76eb76ec967e6f61782a574a0747da7
runsoliv <vilhjalmur1230@gmail.com>
2026-03-12 11:20:07 -0400

    deleted demo folder

--- tests/home.e2e.ts
+++ tests/home.e2e.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('login page shows Google sign-in prompt', async ({ page }) => {
+test('login page shows Google signin', async ({ page }) => {
   await page.goto('http://localhost:4173/login');
 
   await expect(page.getByRole('heading', { name: 'Sign in' })).toBeVisible();
