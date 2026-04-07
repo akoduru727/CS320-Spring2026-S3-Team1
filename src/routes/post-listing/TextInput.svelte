5c755c040e1bcaa18bbd37c6fe9247b1b5cceb66
tahirhusain0805 <tmhusain@umass.edu>
2026-04-07 16:52:59 -0400

    textinput file

---
+++ src/routes/post-listing/TextInput.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  interface Props {
+    class?: string;
+    name: string;
+    placeholder?: string
+    required?: boolean;
+  };
+  
+  const { class: extraClass = "", name, required = false, placeholder = "" }: Props = $props();
+</script>
+
+
+<label class={extraClass}>
+    <p>{name}</p>
+    <input {required} {placeholder} class="w-full bg-zinc-200 py-1 px-2 rounded"/>
+</label>
