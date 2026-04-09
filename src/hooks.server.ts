36ebb4c81469e0ae4138ec100f342e1159d4da11
Toby Adams <tradams@umass.edu>
2026-04-09 09:29:09 -0400

    Fix issue with cookies crash

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -16,9 +16,13 @@ export const handle: Handle = async ({ event, resolve }) => {
     cookies: {
       getAll: () => event.cookies.getAll(),
       setAll: (cookiesToSet) => {
-        cookiesToSet.forEach(({ name, value, options }) => {
-          event.cookies.set(name, value, { ...options, path: "/" });
-        });
+        try {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            event.cookies.set(name, value, { ...options, path: "/" });
+          });
+        } catch (err) {
+            console.error("Error setting cookies:", err);
+          }
       },
     },
   });
