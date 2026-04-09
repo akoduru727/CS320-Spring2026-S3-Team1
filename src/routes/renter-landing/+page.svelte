c1aa10c689ac45738a4aeea7662e71f94aa18fec
kareemix <55011084+kareemix@users.noreply.github.com>
2026-04-09 17:25:46 -0400

    nixed popular listings, stretched listings and filters

--- src/routes/renter-landing/+page.svelte
+++ src/routes/renter-landing/+page.svelte
@@ -39,11 +39,11 @@
 <div class="flex-1 flex overflow-hidden">
   <section class="p-8 space-y-8 flex flex-col flex-1">
 
-      <div class="flex-1 flex gap-5 min-h-0 justify-between">
+      <div class="flex gap-5 min-h-0 justify-center">
         <div>
           <div class="flex ">
-            <div class=" flex flex-col items-center justify-center gap-5 mt-15">
-            <h1 class="text-6xl font-semibold tracking-tighter">
+            <div class=" flex flex-col items-center justify-center gap-5">
+            <h1 class="text-5xl font-semibold tracking-tighter">
               Welcome to your renting dashboard
             </h1>
             <p class="text-zinc-600">
@@ -54,25 +54,12 @@
 
 
       <!--Search Bar-->
-        <div class="mt-15 flex ">
+        <div class="mt-10 flex ">
           <input type="text" placeholder="Search for a listing..."
             class="w-full px-3 py-3 border border-zinc-400 rounded-lg text-zinc-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-red-500 transition-all duration-300 ease-in-out"
             bind:value={searchTerm}>
         </div>
       </div>
-      <Card class="w-1/3 flex flex-col gap-3 pr-0">
-        <div class="flex justify-between pr-6">
-          <h2 class="text-2xl font-medium tracking-tight">
-            Popular Listings
-          </h2>
-        </div>
-
-        <div class="flex w-full flex-col items-stretch gap-3 pr-6 overflow-y-auto">
-          {#each data.listings as listing (listing)}
-            <Listing name={listing.title} address={listing.address} />
-          {/each}
-        </div>
-      </Card>
     </div>
 
 
--- src/routes/renter-landing/Slider.svelte
+++ src/routes/renter-landing/Slider.svelte
@@ -8,14 +8,24 @@
     let {min, max, value = $bindable(), unit}:Props = $props();
     let unitStr:string = $derived.by(() => {
         if(value == 0) {
-            return "Any # " + unit + "s";
+            return "Any # " + unit + "s ";
         } else {
-            return value + " " + unit + (value != 1 ? "s" : "");
+            return value + " " + unit + (value != 1 ? "s " : " ");
         }
     })
+    let unitEmoji = {
+        "Bath": "🛁",
+        "Bed": "🛏️",
+        "Mile": "🚗"
+    };
 </script>
 
-<div class="flex justify-between">
-    <input type=range {min} {max} bind:value={value} class="w-2/3 accent-red-500" />
-    <h2 class="justify-center">{unitStr}</h2>
+<div class="flex-1">
+    <input type=range {min} {max} bind:value={value} class="w-full accent-red-500 mt-5" />
+    <div class="flex mt-3">
+        <h2 class="">{unitStr}</h2>
+        {#each Array(value) as _}
+        <h1>{unitEmoji[unit]}</h1>
+        {/each}
+    </div>
 </div>
